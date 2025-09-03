f85085b2ecdb3ee6cf1a06fc436d3103b27a1caf
kyrylo.garlinsjkyj <kyrylo.garlinsjkyj@renome.ua>
2025-09-03 13:34:38 +0300

    [*] added lab1 & solution

---
+++ lab1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace OOP_HARLINSKYI.lab1
+{
+    public abstract class Figure
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        protected Figure(string name)
+        {
+            _name = name;
+            Console.WriteLine($"Figure {name} created.");
+        }
+
+        ~Figure()
+        {
+            Console.WriteLine($"Figure {_name} destroyed.");
+        }
+
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+
+    public class Circle : Figure
+    {
+        private double _radius;
+
+        public Circle(string name, double radius) : base(name)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Circle circle = new Circle("MyCircle", rnd.Next(1, 10));
+            double area = circle.GetArea();
+
+            Console.WriteLine($"Name: {circle.Name}");
+            Console.WriteLine($"Area: {area}");
+        }
+    }
+}
